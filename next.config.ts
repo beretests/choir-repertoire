59487e6c2049f974657c7737a3d9bb5d6cafc134
Eberechi Omeje <eb.omeje@gmail.com>
2024-11-13 12:17:28 -0600

    remove cloudflare dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,10 @@
 import type { NextConfig } from "next";
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    domains: ['ctk-choir.beretesting.com'], // Add your audio file domains here
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  // images: {
+  //   domains: ['ctk-choir.beretesting.com'], // Add your audio file domains here
+  // }
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
