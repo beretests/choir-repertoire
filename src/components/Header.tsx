c8e41c1f7def8720b72e689464255373cc0e1af7
Eberechi Omeje <eb.omeje@gmail.com>
2025-01-06 14:35:08 -0600

    fix header styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import { useAuth } from '@/contexts/AuthContext';
 import { useSnackbar } from '@/contexts/SnackbarContext';
 import { CircularProgress } from '@mui/material';
 import { useRouter } from 'next/navigation';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
 
 export default function Header() {
   const { user, loading, logout } = useAuth();
@@ -24,15 +26,15 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-gray-100 dark:bg-gray-800 py-4 px-4 sm:px-6">
-      <div className="container mx-auto flex flex-wrap justify-between items-center">
+    <header className="bg-gray-100 dark:bg-gray-800 py-4 sm:px-6">
+      <div className="mx-auto flex flex-col sm:flex-row items-center">
         <Link
           href="/"
           className="text-xl sm:text-2xl font-bold text-black dark:text-white mb-2 sm:mb-0"
         >
           CTK Choir
         </Link>
-        <div className="flex items-right space-x-2 sm:space-x-4 items-center">
+        <div className="flex flex-grow sm:space-x-4 items-center gap-4 justify-around sm:justify-end">
           {loading ? (
             <CircularProgress />
           ) : user?.email ? (
@@ -42,7 +44,8 @@ export default function Header() {
                 onClick={() => handleLogout()}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded"
               >
-                Log out
+                <span className="hidden sm:inline mr-2">Log out</span>
+                <LogoutIcon className="sm:hidden" />
               </button>
             </>
           ) : (
@@ -52,7 +55,8 @@ export default function Header() {
                 href="/login"
                 className="px-3 py-1 sm:px-4 sm:py-2 bg-blue-500 text-white text-sm sm:text-base rounded hover:bg-blue-600"
               >
-                Log In
+                <span className="hidden sm:inline mr-2">Log in</span>
+                <LoginIcon className="sm:hidden" />
               </Link>
             </>
           )}
